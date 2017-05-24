627ffea83e10673a3b1a2815fdace74a1e932517
HrabiaVonTrupca <konrad.gospo@gmail.com>
2017-05-24 12:03:49 +0200

    CT

--- app/src/main/java/com/example/val/pumchat/MainActivity.java
+++ app/src/main/java/com/example/val/pumchat/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.val.pumchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.Console;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Cichy tomasz
     }
 }
