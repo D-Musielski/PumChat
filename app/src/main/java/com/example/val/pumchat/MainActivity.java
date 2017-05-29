1a73cde9e98976979b15625f5ba3f6336ff1d12d
Dominik Musielski <195023@edu.p.lodz.pl>
2017-05-29 20:54:35 +0200

    Main Activity - ready

--- app/src/main/java/com/example/val/pumchat/MainActivity.java
+++ app/src/main/java/com/example/val/pumchat/MainActivity.java
@@ -1,16 +1,77 @@
 package com.example.val.pumchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.quickblox.auth.session.QBSettings;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
 
 import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String APP_ID = "58257";
+    private static final String AUTH_KEY = "G9-sxT5T72WMuGL";
+    private static final String AUTH_SECRET = "k8ubTJqwqXTXgcQ";
+    private static final String ACCOUNT_KEY = "Axv2i8M5jKGzciRxU9kz";
+
+    private Button loginButton;
+    private Button signUpButton;
+
+    private EditText usernameText;
+    private EditText passwordText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //elo
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.registerButton);
+        usernameText = (EditText) findViewById(R.id.loginText);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+
+        qbInit();
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SingUpActivity.class));
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                QBUser qbUser = new QBUser(username, password);
+                QBUsers.signIn(qbUser).performAsync(new QBEntityCallback<QBUser>() {
+                    @Override
+                    public void onSuccess(QBUser qbUser, Bundle bundle) {
+                        Toast.makeText(getBaseContext(), "Success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(QBResponseException e) {
+                        Toast.makeText(getBaseContext(), "Fail"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void qbInit() {
+        QBSettings.getInstance().init(getApplicationContext(), APP_ID, AUTH_KEY, AUTH_SECRET);
+        QBSettings.getInstance().setAccountKey(ACCOUNT_KEY);
     }
 }
